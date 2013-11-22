7769408f822821ca27eff0703e2127f33ab0b56a
jonasterc <jonasterceiro@hotmail.com>
2013-11-22 16:06:25 -0200

    projeto inicio ok

---
+++ js/fontes.js
@@ -0,0 +1,10 @@
+// <![CDATA[
+ Cufon.replace('h1',{ fontFamily: 'HelveticaRounded-Black' });
+ Cufon.replace('h2',{ fontFamily: 'HelveticaRounded-Black' });
+ Cufon.replace('h4',{ fontFamily: 'HelveticaRounded-Black' });
+ Cufon.replace('nav a',{ fontFamily: 'HelveticaRounded-Black' });
+ Cufon.replace('footer',{ fontFamily: 'HelveticaRounded-Black' });
+ Cufon.replace('#modulos ul li',{ fontFamily: 'HelveticaRounded-Black' });
+ Cufon.replace('#telefonescanais',{ fontFamily: 'HelveticaRounded-Black' });
+ Cufon.replace('.telefone',{ fontFamily: 'HelveticaRounded-Black' });         
+ Cufon.replace('#conteudo #textohome p',{ fontFamily: 'HelveticaRounded-Black' });
